d018e0a28541a3389104cc25333d3a126c9db3de
markdjanicki23 <70166748+markdjanicki23@users.noreply.github.com>
2020-10-13 12:45:53 -0400

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -78,8 +78,6 @@ namespace FizzCat.Controllers
             return View();
         }
 
-
-        // This doesnt work yet
         [HttpPost]
         public IActionResult SunsetHills(int input0, int input1, int input2, int input3, int input4)
         {
